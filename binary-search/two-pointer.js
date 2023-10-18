4e061fdd58a29279d36c4dda709a71eac54a3ef3
Suraj BG <surajbg@Surajs-MacBook-Air.local>
2023-10-18 19:01:00 +0530

    feat: Two Sum problem for sorted array

--- binary-search/two-pointer.js
+++ binary-search/two-pointer.js
@@ -24,3 +24,18 @@ const arr = [20, 40, 60, 80, 90, 120, 240];
 const target = 210;
 const result = two_sum(arr, target);
 console.log(result);
+
+//Two Sum Problem for Sorted Array
+const twoSum = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    if (nums[left] + nums[right] === target) {
+      return [left, right];
+    } else if (nums[left] + nums[right] < target) {
+      left += 1;
+    } else right -= 1;
+  }
+};
+const nums = [2, 7, 11, 15];
+const t = 9;
