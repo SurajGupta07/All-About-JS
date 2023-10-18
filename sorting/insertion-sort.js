67451593b868efdb402211de8a299fe6facafaea
Suraj BG <surajbg@Surajs-MacBook-Air.local>
2023-10-18 14:52:32 +0530

    insertion sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,15 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > key; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+};
+
+const arr = [75, 90, 100, 95, 85, 80];
+const result = insertionSort(arr);
+console.log(insertionSort([75, 90, 100, 95, 85, 80]));
