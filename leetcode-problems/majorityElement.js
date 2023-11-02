0985e8bdd396f4a7d65bc1e30380bc0fd51c4288
Suraj BG <surajbg@Surajs-MacBook-Air.local>
2023-11-02 21:23:06 +0530

    feat: Boyer-Moore(Majority element)

---
+++ leetcode-problems/majorityElement.js
@@ -0,0 +1,40 @@
+//Time complexity => O(n)
+//Space complexity => O(1)
+//Method defination of isMajorityElement
+const isMajorityElement = (nums, candidate) => {
+  let count = 0;
+  let size = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    if (candidate === nums[i]) {
+      count++;
+    }
+  }
+  if (count > Math.floor(size / 2)) return true;
+  else false;
+};
+
+//Method defination of findCandidate
+const findCandidate = (nums) => {
+  let candidate = null;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+    } else count += arr[i] === candidate ? 1 : -1;
+  }
+  return candidate;
+};
+
+const printMajorityElement = (nums) => {
+  let candidate = findCandidate(nums);
+  if (isMajorityElement(nums, candidate)) {
+    console.log("Majority Element is:", candidate);
+  } else {
+    console.log("No Majority element exsists in an array");
+  }
+};
+
+// Driver code
+const nums = [2, 2, 1, 1, 1, 2, 2];
+printMajorityElement(nums);
