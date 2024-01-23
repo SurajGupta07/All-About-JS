60e74e3c0db80e3c9fd2653e9bcbe18a2d3259d8
SurajGupta07 <bgsuraj07@gmail.com>
2024-01-23 16:54:56 +0530

    findMinAndMaxSum.js

---
+++ assignment/findMaxAndMinSum.js
@@ -0,0 +1,12 @@
+const findMaxAndMinSum = (arr, k) => {
+  arr.sort((a, b) => a - b);
+
+  const minSum = arr
+    .slice(0, arr.length - k)
+    .reduce((prev, cur) => prev + cur, 0);
+  const maxSum = arr.slice(k, arr.length).reduce((prev, cur) => prev + cur, 0);
+  return [minSum, maxSum];
+};
+
+const res = findMaxAndMinSum([5, 4, 3, 2, 1], 1);
+console.log(res);
