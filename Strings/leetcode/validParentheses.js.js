737d5651028e3ed817a73cc79dbfacb5be912b0f
SurajGupta07 <bgsuraj07@gmail.com>
2024-02-07 21:13:40 +0530

    valid parentheses

---
+++ Strings/leetcode/validParentheses.js.js
@@ -0,0 +1,23 @@
+const checkIsValidParentheses = (str) => {
+  let stack = [];
+  for (let c of str) {
+    if (c == "(" || c === "[" || c === "{") {
+      stack.push(c);
+    } else {
+      if (
+        !stack.length ||
+        (c === ")" && stack[stack.length - 1] !== "(") ||
+        (c === "}" && stack[stack.length - 1] !== "{") ||
+        (c === "]" && stack[stack.length - 1] !== "[")
+      ) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return !stack.length;
+};
+
+console.log(checkIsValidParentheses("()"));
+console.log(checkIsValidParentheses("()[]{}"));
+console.log(checkIsValidParentheses("(]"));
