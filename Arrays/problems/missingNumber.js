9ed9967226f3a71e443924285105f50d8d0dd29d
SurajGupta07 <bgsuraj07@gmail.com>
2024-03-15 19:26:04 +0530

    missing number js

---
+++ Arrays/problems/missingNumber.js
@@ -0,0 +1,10 @@
+const missingNumber = (arr) => {
+    arr.sort((a,b) => a - b)
+    if(arr[0] !== 0) return 0
+    if(arr[arr.length - 1] !== arr.length) return arr.length
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] !== i) return i
+    }
+}
+
+console.log(missingNumber([0,1,2,4,5]))
