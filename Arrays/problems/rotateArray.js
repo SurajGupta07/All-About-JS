f7e9fa8c96d9d1bf134d8fcd623f1773579b8d4c
SurajGupta07 <bgsuraj07@gmail.com>
2024-03-15 12:58:29 +0530

    array rotations

--- Arrays/problems/rotateArray.js
+++ Arrays/problems/rotateArray.js
@@ -18,3 +18,29 @@ const rotateArray = (arr, k) => {
 
 const res = rotateArray([1, 2, 3, 4, 5], 2);
 console.log(res);
+
+const leftRotation = (arr) => {
+  let temp = arr[0];
+  for(let i = 0; i <= arr.length - 1; i++) {
+    arr[i] = arr[i+1]
+  }
+  arr[arr.length - 1] = temp
+  return arr
+}
+
+const array = [1,2,3,4,5]
+console.log(leftRotation(array))
+
+const rotateArrayKPlaces = (arr, k) => {
+  const rotatedArray = [];
+  for(let i = 0; i < k; i++) {
+    rotatedArray.push(arr[arr.length - i - 1])
+  }
+  for(let i = 0; i < arr.length - k; i++) {
+    rotatedArray.push(arr[i])
+  }
+  return rotatedArray
+}
+
+const elements = [1,2,3,4,5,6,7]
+console.log(rotateArrayKPlaces(elements, 3))
