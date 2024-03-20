a94f60aff56911c2ab1396a0522384a6b966c4f3
SurajGupta07 <bgsuraj07@gmail.com>
2024-03-20 21:18:33 +0530

    hashMap problems

---
+++ Strings/problems/stringFrequency.js
@@ -0,0 +1,14 @@
+const stringFrequency = (arr) => {
+    const hashMap = new Map()
+    for(let element of arr) {
+        if(hashMap.has(element)) {
+           hashMap.set(element, hashMap.get(element) + 1) 
+        } else {
+            hashMap.set(element, 1)
+        }
+    }
+    const maxCount = Math.max(...hashMap.values())
+    return new Map([...hashMap].filter(([key, value]) => value === maxCount))
+}
+
+console.log(stringFrequency(['a', 'b', 'c', 'd', 'a', 'c']))
