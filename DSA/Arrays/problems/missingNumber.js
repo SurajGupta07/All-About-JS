b27f9f9773fdc059351c552bcfe2d34a5998d9a9
SurajGupta07 <bgsuraj07@gmail.com>
2024-09-12 21:55:43 +0530

    missing number

--- DSA/Arrays/problems/missingNumber.js
+++ DSA/Arrays/problems/missingNumber.js
@@ -1,10 +1,10 @@
 const missingNumber = arr => {
-  arr.sort((a, b) => a - b);
-  if (arr[0] !== 0) return 0;
-  if (arr[arr.length - 1] !== arr.length) return arr.length;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== i) return i;
+  let total = (arr.length * (arr.length + 1)) / 2;
+  let sum = 0;
+  for (let ele of arr) {
+    sum += ele;
   }
+  return total - sum;
 };
 
-console.log(missingNumber([0, 1, 2, 4, 5]));
+console.log(missingNumber([1, 2, 0, 4, 5]));
