5d10590569775756c6ed3809eaa8eed78df16c54
SurajGupta07 <bgsuraj07@gmail.com>
2024-10-09 22:30:34 +0530

    consequtive ones

--- DSA/Arrays/problems/maximumOnes.js
+++ DSA/Arrays/problems/maximumOnes.js
@@ -9,3 +9,27 @@ const maxConsecutiveOnes = arr => {
 };
 
 console.log(maxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
+
+const longestSubArrays = (arr, k) => {
+  let sum = 0,
+    maxLength = 0,
+    subArray = [];
+  const map = new Map([[0, -1]]);
+
+  arr.forEach((num, i) => {
+    sum += num;
+    const startIdx = map.get(sum - k);
+
+    if (startIdx !== undefined && i - startIdx > maxLength) {
+      maxLength = i - startIdx;
+      subArray = arr.slice(startIdx + 1, i + 1);
+    }
+
+    map.set(sum, map.get(sum) ?? i);
+  });
+
+  return subArray;
+};
+
+console.log(longestSubArrays([2, 3, 5], 5));
+console.log(longestSubArrays([2, 3, 5, 1, 9], 10));
